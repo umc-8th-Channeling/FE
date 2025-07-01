c3bc44fa6e2b984230a8a48cfe508262f4dfc66b
Sowon Kim <sso626262@naver.com>
2025-07-02 03:50:43 +0900

    feat: 타이포 및 컬러 시스템, 기본 배경 색상 global css 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,36 @@
 function App() {
-  return (
-    <>
-      <div className="text-red-500">channeling</div>
-    </>
-  );
+    return (
+        <>
+            <p>타이포 및 컬러시스템</p>
+            <div className="grid grid-cols-2">
+                <div>
+                    <div className="text-primary-50 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-100 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-200 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-300 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-400 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-500 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-600 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-700 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-800 text-5xl font-bold">Dark/Primary</div>
+                    <div className="text-primary-900 text-5xl font-bold">Dark/Primary</div>
+                </div>
+
+                <div>
+                    <div className="text-gray-50 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-100 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-200 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-300 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-400 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-500 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-600 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-700 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-800 text-5xl font-bold">Dark/Grayscale</div>
+                    <div className="text-gray-900 text-5xl font-bold">Dark/Grayscale</div>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
