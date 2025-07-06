c1a0c3fc9bb71f8e2e4fe7ef09f4ffb06069d009
sejeong223 <sejeonggim68@gmail.com>
2025-07-06 12:36:42 +0900

    feat: 툴팁 애니메이션 종료 시 자동 제거 기능 추가

--- frontend/src/components/common/Navbar/NavbarToolTip.tsx
+++ frontend/src/components/common/Navbar/NavbarToolTip.tsx
@@ -1,18 +1,29 @@
+import { useEffect, useState } from "react";
 import ToolTipIcon from "../../../assets/icons/tooltip.svg";
 
-export const ToolTipBubble = (): React.ReactElement => {
-    return (
-        <div
-            className="
-                absolute left-4 bottom-16
-                w-[140px] h-[38px] pr-[9px]
-                flex items-center
-            "
-        >
-            <div className="w-24 flex-none"></div>
+export const ToolTipBubble = (): React.ReactElement | null => {
+  const [visible, setVisible] = useState(true);
 
-                <img src={ToolTipIcon} alt="툴팁" id="tooltip" />
-            </div>
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 8000);
 
-    );
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!visible) return null;
+
+  return (
+    <div
+      className="
+        absolute left-4 bottom-16
+        w-[140px] h-[38px] pr-[9px]
+        flex items-center
+      "
+    >
+      <div className="w-24 flex-none"></div>
+      <img src={ToolTipIcon} alt="툴팁" id="tooltip" />
+    </div>
+  );
 };
