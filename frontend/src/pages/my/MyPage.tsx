a92639613db77518d0c29893670afa85cc567f8a
yoonvinjeong <anaabc1004@naver.com>
2025-07-09 00:20:02 +0900

    feat: status box 아이콘 추가

--- frontend/src/pages/my/MyPage.tsx
+++ frontend/src/pages/my/MyPage.tsx
@@ -1,5 +1,15 @@
 import ProfileCard from './_components/profileCard'
 import { type Stats } from './profile'
+import StatsCard from './_components/statsCard'
+import Targetbox from './_components/targetbox'
+import Conceptbox from './_components/conceptbox'
+import Videolist from './_components/videolist'
+import View from '../../assets/icons/view.svg?react'
+import Like from '../../assets/icons/like.svg?react'
+import People from '../../assets/icons/people.svg?react'
+import Share from '../../assets/icons/share.svg?react'
+import Videos from '../../assets/icons/videos.svg?react'
+import Comment from '../../assets/icons/comment.svg?react'
 
 const profileData = {
     imageUrl: 'https://placekitten.com/200/200',
@@ -8,11 +18,6 @@ const profileData = {
     channelUrl: 'https://youtube.com/@찰스엔터',
     joinDate: '2025. 6. 29.',
 }
-
-import StatsCard from './_components/statsCard'
-import Targetbox from './_components/targetbox'
-import Conceptbox from './_components/conceptbox'
-import Videolist from './_components/videolist'
 
 const statsMeta = [
     { key: 'views', title: '조회수' },
@@ -33,6 +38,15 @@ const stats: Stats = {
     comments: 400,
 }
 
+const statIcons = {
+    views: <View />,
+    likes: <Like />,
+    subscribers: <People />,
+    shares: <Share />,
+    videos: <Videos />,
+    comments: <Comment />,
+}
+
 export default function Mypage() {
     return (
         <div className="flex bg-surface-base">
@@ -42,7 +56,12 @@ export default function Mypage() {
                         <ProfileCard profile={profileData} />
                         <div className="w-[588px] h-[220px] grid grid-cols-3 gap-[24px] ">
                             {statsMeta.map((meta) => (
-                                <StatsCard key={meta.key} title={meta.title} value={stats[meta.key]} />
+                                <StatsCard
+                                    key={meta.key}
+                                    title={meta.title}
+                                    value={stats[meta.key]}
+                                    icon={statIcons[meta.key]}
+                                />
                             ))}
                         </div>
                     </div>
