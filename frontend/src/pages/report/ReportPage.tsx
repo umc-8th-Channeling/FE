71529404aeb127775aafcb7eb9541a3028cbd543
Sowon Kim <sso626262@naver.com>
2025-07-11 05:57:04 +0900

    feat: 개요 탭 도넛 그래프 제외 1차 UI 구현

--- frontend/src/pages/report/ReportPage.tsx
+++ frontend/src/pages/report/ReportPage.tsx
@@ -1,6 +1,7 @@
 import Tabs from '../../components/Tabs'
 import { TabOverview, TabAnalysis, TabIdea, VideoSummary } from './_components'
 import { VIDEO } from './dummy'
+import Refresh from '../../assets/icons/refresh_2.svg?react'
 
 const TABS = [
     { label: '개요', component: <TabOverview /> },
@@ -15,6 +16,16 @@ export default function ReportPage() {
         <div className="px-[76px] py-20 space-y-10 bg-gray-50">
             <VideoSummary video={video} />
             <Tabs tabs={TABS} />
+
+            <button
+                onClick={() => console.log('Report Page: refresh button')}
+                className="
+                    cursor-pointer absolute bottom-6 right-6 p-4 rounded-2xl 
+                    border border-primary-600 bg-primary-500 shadow-[0_0_8px_0_var(--color-primary-500)]
+                "
+            >
+                <Refresh />
+            </button>
         </div>
     )
 }
--- frontend/src/pages/report/_components/overview/CommentFeedback.tsx
+++ frontend/src/pages/report/_components/overview/CommentFeedback.tsx
@@ -1,9 +1,43 @@
+import { DoughnutChart } from '../../../../components/chart'
+import Tabs from '../../../../components/Tabs'
 import { TitledSection } from '../TitledSection'
+import { OVERVIEW_COMMENTS as COMMENTS } from '../../dummy'
+
+const Comments = ({ comments }: { comments: string[] }) => {
+    return (
+        <div className="flex flex-col gap-4">
+            {comments.map((comment, index) => (
+                <div
+                    key={index}
+                    className="px-4 py-2 rounded-lg bg-surface-elevate-l2 text-[16px] leading-[150%] tracking-[-0.4px]"
+                >
+                    {comment}
+                </div>
+            ))}
+        </div>
+    )
+}
+
+const TABS = COMMENTS.map(({ label, comments }) => ({
+    label,
+    component: <Comments comments={comments} />,
+}))
 
 export const CommentFeedback = () => {
     return (
         <TitledSection title="댓글 반응">
-            <div>CommentFeedback</div>
+            <div className="flex flex-col desktop:flex-row justify-center items-center desktop:items-start p-6 gap-8 border border-gray-200 rounded-lg bg-surface-elevate-l1 overflow-hidden">
+                <div className="desktop:min-w-[448px]">
+                    <DoughnutChart labels={['긍정', '부정', '중립', '조언 및 의견']} data={[25, 25, 6, 44]} />
+                </div>
+
+                <Tabs
+                    tabs={TABS}
+                    textStyle="text-[16px] leading-[150%] tracking-[-0.4px]"
+                    bgColor="bg-surface-elevate-l2"
+                    spaceY="space-y-4"
+                />
+            </div>
         </TitledSection>
     )
 }
