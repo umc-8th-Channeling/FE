14af0224f06a9c03b159c86248c45f11ec31964e
sejeong223 <sejeonggim68@gmail.com>
2025-07-11 03:35:16 +0900

    feat: SettingInput 컴포넌트 생성 및 스타일 적용

--- frontend/src/pages/setting/components/SettingInput.tsx
+++ frontend/src/pages/setting/components/SettingInput.tsx
@@ -1,8 +1,11 @@
-export default function Input({ ...props }) {
+export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
     return (
-        <input
-            {...props}
-            className="w-full px-3 py-2 rounded-md bg-neutral-800 text-white border border-neutral-600 focus:outline-none focus:ring-2 focus:ring-white"
-        />
+        <div
+            className="flex flex-col items-start gap-6
+        w-[561px] h-[36px] p-2 rounded-lg
+        bg-[rgba(255,255,255,0.10)]"
+        >
+            <input {...props} className="w-full h-full font-caption text-[#A8A8A8]" />
+        </div>
     )
 }
