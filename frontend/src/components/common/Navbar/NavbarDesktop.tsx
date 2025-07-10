4b477c4a5b235f3fc4b8fc2ae630ec6ff62648d5
sejeong223 <sejeonggim68@gmail.com>
2025-07-10 20:53:35 +0900

    fix: 로그인 아이콘 클릭 시 모달로 이동하도록 변경

--- frontend/src/components/common/Navbar/NavbarDesktop.tsx
+++ frontend/src/components/common/Navbar/NavbarDesktop.tsx
@@ -1,37 +1,47 @@
-import ChannelingIcon from '../../../assets/icons/channeling.svg';
-import { NavbarLink } from './NavbarLink';
-import { TOP_LINKS, BOTTOM_LINKS } from './navbarLinks';
-import { NavbarContainer } from './NavbarContainer';
-import { ToolTipBubble } from './NavbarToolTip';
+import ChannelingIcon from '../../../assets/icons/channeling.svg'
+import { NavbarLink } from './NavbarLink'
+import { TOP_LINKS, BOTTOM_LINKS } from './navbarLinks'
+import { NavbarContainer } from './NavbarContainer'
+import { ToolTipBubble } from './NavbarToolTip'
+import { useState } from 'react'
+import Modal from '../../Modal'
 // import { NavbarUserInfo } from './NavbarUserInfo';
 
 export const NavbarDesktop = (): React.ReactElement => {
-  return (
-    <NavbarContainer>
-  <div className="flex flex-col justify-between h-full w-full">
-    <div className="flex flex-col items-center gap-22">
-      <img src={ChannelingIcon} alt="Channeling 로고" className="w-12 h-12" />
-      <div className="flex flex-col items-center gap-6">
-        <NavbarLink {...TOP_LINKS.find(link => link.to === "/report")!} />
-        <div className="flex flex-col items-center gap-1">
-          {TOP_LINKS.filter(link => link.to !== "/report").map(link => (
-            <NavbarLink key={link.to} {...link} />
-          ))}
-        </div>
-      </div>
-    </div>
-    <div className="flex flex-col items-center">
-      {/* 로그인 성공 시 아이콘 변경
+    const [showLoginModal, setShowLoginModal] = useState(false)
+
+    const handleLoginClick = () => setShowLoginModal(true)
+    const handleCloseModal = () => setShowLoginModal(false)
+
+    return (
+        <>
+            <NavbarContainer>
+                <div className="flex flex-col justify-between h-full w-full">
+                    <div className="flex flex-col items-center gap-22">
+                        <img src={ChannelingIcon} alt="Channeling 로고" className="w-12 h-12" />
+                        <div className="flex flex-col items-center gap-6">
+                            <NavbarLink {...TOP_LINKS.find((link) => link.to === '/report')!} />
+                            <div className="flex flex-col items-center gap-1">
+                                {TOP_LINKS.filter((link) => link.to !== '/report').map((link) => (
+                                    <NavbarLink key={link.to} {...link} />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center">
+                        {/* 로그인 성공 시 아이콘 변경
       {user ? (
         <NavbarUserInfo user={user} />
       ) : */}
-      {BOTTOM_LINKS.map(link => (
-        <NavbarLink key={link.to} {...link} />
-      ))}
-    </div>
-  </div>
-  <ToolTipBubble />
-</NavbarContainer>
+                        {BOTTOM_LINKS.map((link) => (
+                            <NavbarLink key={link.alt} {...link} onLoginClick={handleLoginClick} />
+                        ))}
+                    </div>
+                </div>
+                <ToolTipBubble />
+            </NavbarContainer>
 
-  );
-};
+            {showLoginModal && <Modal title="로그인" onClose={handleCloseModal} />}
+        </>
+    )
+}
