d9ced925a851013335587d6170323c17c30a025f
yoonvinjeong <anaabc1004@naver.com>
2025-07-10 20:08:28 +0900

    style: 모달 버튼 위치 수정

--- frontend/src/pages/my/_components/videolist.tsx
+++ frontend/src/pages/my/_components/videolist.tsx
@@ -46,19 +46,18 @@ export default function Videolist() {
                             description="‘저 맘먹으면 광태님 꼬실 수 있어요ㅎ [월간데이트 4월호 ] (ENG / JP SUB)’을 유튜버님의 타겟과 컨셉을 고려하여 분석해요."
                             onClose={() => setOpen(false)}
                         >
-                            <div className="flex justify-between w-[214px] h-[40px] mt-[24px] ml-[248px]">
-                                <button
-                                    className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] cursor-pointer"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    취소
-                                </button>
-                                <button
-                                    className="w-[103px] h-[40px] text-[16px] font-bold text-gray-900 bg-primary-500 rounded-[16px] cursor-pointer"
-                                    onClick={getReport}
-                                >
-                                    리포트 받기
-                                </button>
+                            <div className="flex justify-end">
+                                <div className="flex justify-between w-[214px] h-[40px] mt-[24px] items-end">
+                                    <button
+                                        className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        취소
+                                    </button>
+                                    <button className="w-[103px] h-[40px] text-[16px] font-bold text-gray-900 bg-primary-500 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer">
+                                        리포트 받기
+                                    </button>
+                                </div>
                             </div>
                         </Modal>
                     )}
@@ -73,16 +72,18 @@ export default function Videolist() {
                             description="‘저 맘먹으면 광태님 꼬실 수 있어요ㅎ [월간데이트 4월호 ] (ENG / JP SUB)’을 유튜버님의 타겟과 컨셉을 고려하여 분석해요."
                             onClose={() => setOpen(false)}
                         >
-                            <div className="flex justify-between w-[214px] h-[40px] mt-[24px] ml-[248px]">
-                                <button
-                                    className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer"
-                                    onClick={() => setOpen(false)}
-                                >
-                                    취소
-                                </button>
-                                <button className="w-[103px] h-[40px] text-[16px] font-bold text-gray-900 bg-primary-500 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer">
-                                    리포트 받기
-                                </button>
+                            <div className="flex justify-end">
+                                <div className="flex justify-between w-[214px] h-[40px] mt-[24px] items-end">
+                                    <button
+                                        className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer"
+                                        onClick={() => setOpen(false)}
+                                    >
+                                        취소
+                                    </button>
+                                    <button className="w-[103px] h-[40px] text-[16px] font-bold text-gray-900 bg-primary-500 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer">
+                                        리포트 받기
+                                    </button>
+                                </div>
                             </div>
                         </Modal>
                     )}
