36719b76ca8a03bbc747cc154f02a21e97301b53
yoonvinjeong <anaabc1004@naver.com>
2025-07-11 16:45:23 +0900

    style: 레포트 받기 모달 수정
    - 너비를 설정했습니다
    - 불필요한 스타일링 속성을 제거했습니다

--- frontend/src/pages/my/_components/videolist.tsx
+++ frontend/src/pages/my/_components/videolist.tsx
@@ -45,9 +45,10 @@ export default function Videolist() {
                             title="해당 영상에 대한 리포트를 받아 보시겠어요?"
                             description="‘저 맘먹으면 광태님 꼬실 수 있어요ㅎ [월간데이트 4월호 ] (ENG / JP SUB)’을 유튜버님의 타겟과 컨셉을 고려하여 분석해요."
                             onClose={() => setOpen(false)}
+                            className="w-[486px]"
                         >
                             <div className="flex justify-end">
-                                <div className="flex justify-between w-[214px] h-[40px] mt-[24px] items-end">
+                                <div className="flex justify-between w-[214px] h-[40px] items-end">
                                     <button
                                         className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer"
                                         onClick={() => setOpen(false)}
@@ -74,9 +75,10 @@ export default function Videolist() {
                             title="해당 영상에 대한 리포트를 받아 보시겠어요?"
                             description="‘저 맘먹으면 광태님 꼬실 수 있어요ㅎ [월간데이트 4월호 ] (ENG / JP SUB)’을 유튜버님의 타겟과 컨셉을 고려하여 분석해요."
                             onClose={() => setOpen(false)}
+                            className="w-[486px]"
                         >
                             <div className="flex justify-end">
-                                <div className="flex justify-between w-[214px] h-[40px] mt-[24px] items-end">
+                                <div className="flex justify-between w-[214px] h-[40px] items-end">
                                     <button
                                         className="w-[101px] h-[40px] text-[16px] font-bold text-gray-600 border-[1px] border-gray-300 rounded-[16px] leading-[150%] tracking-[-0.4px] cursor-pointer"
                                         onClick={() => setOpen(false)}
