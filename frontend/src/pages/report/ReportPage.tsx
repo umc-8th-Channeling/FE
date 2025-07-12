d771891e15aa82212a2232fb9c53d148a084c633
Sowon Kim <sso626262@naver.com>
2025-07-12 18:59:28 +0900

    feat: 레포트 페이지 데모 데이터 기반 알림 모달 추가

--- frontend/src/pages/report/ReportPage.tsx
+++ frontend/src/pages/report/ReportPage.tsx
@@ -1,8 +1,8 @@
 import Tabs from '../../components/Tabs'
-import { TabOverview, TabAnalysis, TabIdea, VideoSummary } from './_components'
+import { TabOverview, TabAnalysis, TabIdea, VideoSummary, GuestModal } from './_components'
 import { VIDEO } from './dummy'
 import Refresh from '../../assets/icons/refresh_2.svg?react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Modal from '../../components/Modal'
 
 const TABS = [
@@ -14,15 +14,23 @@ const TABS = [
 export default function ReportPage() {
     const [activeTab, setActiveTab] = useState(TABS[0].label)
     const [isOpenUpdateModal, setIsOpenUpdateModal] = useState(false)
+    const [isOpenGuestModal, setIsOpenGuestModal] = useState(false) // 전역 상태 전환 필요
+
     const video = VIDEO
 
-    const handleOpenModalClick = () => setIsOpenUpdateModal(!isOpenUpdateModal)
+    useEffect(() => {
+        const isGuest = !localStorage.getItem('token')
+        setIsOpenGuestModal(isGuest)
+    }, [])
+
+    const handleOpenUpdateModalClick = () => setIsOpenUpdateModal(!isOpenUpdateModal)
+    const handleOpenGuestModalClick = () => setIsOpenGuestModal(!isOpenGuestModal)
 
     const handleUpdateClick = () => {
         console.log('Report Page: update')
 
         setActiveTab(TABS[0].label) // 업데이트 후 탭 초기화
-        handleOpenModalClick()
+        handleOpenUpdateModalClick()
     }
 
     return (
@@ -36,11 +44,11 @@ export default function ReportPage() {
                     <Modal
                         title="리포트를 업데이트 하시겠어요?"
                         description={`현재 시각 기준으로 다시 분석합니다. \n기존 아이디어 카드는 삭제되며 복구할 수 없습니다. \n필요한 카드는 미리 북마크해 주세요.`}
-                        onClose={handleOpenModalClick}
+                        onClose={handleOpenUpdateModalClick}
                     >
                         <div className="flex justify-end gap-2">
                             <button
-                                onClick={handleOpenModalClick}
+                                onClick={handleOpenUpdateModalClick}
                                 className="cursor-pointer min-w-[103px] px-4 py-2 rounded-2xl border border-gray-300 bg-transparent"
                             >
                                 취소
@@ -56,9 +64,12 @@ export default function ReportPage() {
                 )}
             </div>
 
+            {/* 데모 데이터 기반 레포트 알림 모달 */}
+            {isOpenGuestModal && <GuestModal onClose={handleOpenGuestModalClick} />}
+
             {/* 리포트 업데이트 버튼 */}
             <button
-                onClick={handleOpenModalClick}
+                onClick={handleOpenUpdateModalClick}
                 className="
                     cursor-pointer fixed bottom-6 right-6 p-4 rounded-2xl 
                     border border-primary-600 bg-primary-500 shadow-[0_0_8px_0_var(--color-primary-500)]
---
+++ frontend/src/pages/report/_components/GuestModal.tsx
@@ -0,0 +1,30 @@
+import * as motion from 'motion/react-client'
+import X from '../../../assets/icons/X.svg?react'
+
+export const GuestModal = ({ onClose }: { onClose: () => void }) => {
+    return (
+        <div className="fixed top-0 flex justify-center w-full">
+            <motion.div
+                initial={{ opacity: 0, y: 0 }}
+                animate={{ opacity: 1, y: [0, 32, 24, 28] }}
+                transition={{
+                    duration: 0.8,
+                    ease: 'easeOut',
+                }}
+                className="
+                    flex flex-row items-center px-4 py-2 space-x-2 rounded-lg bg-surface-elevate-l2
+                    whitespace-pre-line tablet:whitespace-nowrap text-center text-[16px] leading-[150%] tracking-[-0.4px]
+                "
+            >
+                <span>
+                    이 리포트는 <span className="font-bold text-primary-500">데모 데이터</span>를 기반으로 제공됩니다.
+                    {'\n'}로그인하시면 내 영상의 리포트를 확인하실 수 있습니다.
+                </span>
+
+                <button onClick={onClose} className="cursor-pointer">
+                    <X />
+                </button>
+            </motion.div>
+        </div>
+    )
+}
