b3293d2315cc3cc1284614b484504df86e32d94b
띵 <wkdaudns12@gmail.com>
2025-07-19 00:54:58 +0900

    feat: hover시 X 아이콘 구현

--- frontend/src/pages/library/_components/RecentReportCard.tsx
+++ frontend/src/pages/library/_components/RecentReportCard.tsx
@@ -1,12 +1,18 @@
 import { memo } from 'react'
 import type { LibraryItem } from '../../../types/library'
+import { X } from 'lucide-react'
 
 export default memo(function RecentReportCard({ item }: { item: LibraryItem }) {
     return (
-        <div className="rounded-lg overflow-hidden bg-transparent ">
-            <p className="text-sm font-normal leading-[19.6px] tracking-[-0.35px] text-gray-600">
-                업데이트 : {item.updatedAt}
-            </p>
+        <div className="relative group rounded-lg overflow-hidden bg-transparent ">
+            <div className="flex items-center justify-between">
+                <p className="text-sm font-normal leading-[19.6px] tracking-[-0.35px] text-gray-600">
+                    업데이트 : {item.updatedAt}
+                </p>
+                <button className="absolute w-6 h-6 -right-[4px] hidden group-hover:block ">
+                    <X className="w-full h-full fill-gray-900 " />
+                </button>
+            </div>
 
             <img src={item.thumbnail} alt={item.title} className="w-full aspect-video object-cover mt-2 mb-2" />
 
