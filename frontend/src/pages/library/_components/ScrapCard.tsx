79387fccdac584f301703919b785f291612efe05
띵 <wkdaudns12@gmail.com>
2025-07-20 18:31:48 +0900

    style: hover시 border-300 추가, 태그 색상 변경

--- frontend/src/pages/library/_components/ScrapCard.tsx
+++ frontend/src/pages/library/_components/ScrapCard.tsx
@@ -7,7 +7,7 @@ export default memo(function ScrapCard({ item }: { item: ScrapItem }) {
     const [isFilled, setIsFilled] = useState(true) // true면 채워짐
 
     return (
-        <div className="relative w-full p-5 rounded-lg border border-gray-200 bg-gray-100 hover:bg-gray-200 duration-300 transition">
+        <div className="relative w-full p-5 rounded-lg border border-gray-200 bg-gray-100 hover:bg-gray-200 hover:border-gray-300 duration-300 transition">
             {isFilled ? (
                 <BookmarkActive
                     className="absolute top-5 right-5 w-5 h-5 cursor-pointer"
@@ -25,7 +25,7 @@ export default memo(function ScrapCard({ item }: { item: ScrapItem }) {
 
             <div className="flex flex-wrap gap-2 text-base font-medium leading-[24px] tracking-[-0.4px]">
                 {item.hashtags.map((tag) => (
-                    <span key={tag} className="text-xs text-gray-900 bg-primary-300 px-2 py-0.5 rounded-xs">
+                    <span key={tag} className="text-xs text-gray-900 bg-primary-opacity50 px-2 py-0.5 rounded-xs">
                         #{tag}
                     </span>
                 ))}
