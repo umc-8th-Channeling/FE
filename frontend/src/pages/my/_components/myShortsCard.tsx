3ba574e0a2d663334d3b1d740a33ce870d7b6803
Sowon Kim <sso626262@naver.com>
2025-07-23 01:32:26 +0900

    style: 저장소, 내 채널 페이지 반응형 스타일 수정

--- frontend/src/pages/my/_components/myShortsCard.tsx
+++ frontend/src/pages/my/_components/myShortsCard.tsx
@@ -5,11 +5,11 @@ interface MyShortsCardProps {
 export default function MyShortsCard({ onClick }: MyShortsCardProps) {
     return (
         <div
-            className="flex w-[190px] h-[371px] flex-col items-center tablet:gap-x-[9px] desktop:gap-[8px] shrink-0"
+            className="flex flex-col h-fit items-center tablet:gap-x-[9px] desktop:gap-[8px] shrink-0"
             onClick={onClick}
         >
-            <div className="w-[192px] h-[289px] rounded-[8px] aspect-[192/289] bg-primary-800"></div>
-            <div className=" w-[190px] h-[371px] gap-[4px] mt-[8px] items-start">
+            <div className="w-full aspect-[192/289] rounded-[8px] bg-primary-800"></div>
+            <div className="w-full gap-1 mt-[8px] items-start">
                 <div className="max-h-[50px] text-gray-900 text-[18px] multi-line-ellipsis font-bold leading-[140%] tracking-[-0.45px]">
                     통행을 방해하는 불법 주차 차량을 보았다. 트윈스의
                     선택은?[LP]ddddddddddddddddddpppppppppppppppppppppppp
