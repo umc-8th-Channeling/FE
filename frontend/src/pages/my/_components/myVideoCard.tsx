d8baa45b56d1a80165fd8981cd00615e5471c40b
yoonvinjeong <anaabc1004@naver.com>
2025-07-22 19:35:39 +0900

    feat: 영상 제목 말줄임 표시 처리
    - 영상 제목이 2줄을 넘어갈 경우 ...로 보이도록 처리했습니다

--- frontend/src/pages/my/_components/myVideoCard.tsx
+++ frontend/src/pages/my/_components/myVideoCard.tsx
@@ -7,8 +7,9 @@ export default function MyVideoCard({ onClick }: MyVideoCardProps) {
         <div className="flex flex-col h-[240px] items-center gap-[8px] " onClick={onClick}>
             <div className="w-[282px] h-[158px] shrink-0 aspect-[141/79] rounded-[8px] bg-primary-600"></div>
             <div className="flex flex-col items-start gap-[4px] w-full ">
-                <div className="max-h-[50px] self-stretch text-gray-900 text-[18px] font-bold overflow-ellipsis leading-[140%] tracking-[-0.45px]">
-                    저 드디어 독립했어요!
+                <div className="max-w-[282px] max-h-[50px] self-stretch text-gray-900 text-[18px] font-bold multi-line-ellipsis leading-[140%] tracking-[-0.45px]">
+                    통행을 방해하는 불법 주차 차량을 보았다. 트윈스의
+                    선택은?[LP]ddddddddddddddddddpppppppppppppppppppppppp
                 </div>
                 <div className="self-stretch text-gray-600 text-[14px] font-normal leading-[140%] tracking-[-0.35px]">
                     조회수 17만회 · 3년 전
