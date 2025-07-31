802f10bc29e5f67fcf5ecd3321a13b038186a719
sejeong223 <sejeonggim68@gmail.com>
2025-07-31 23:20:58 +0900

    feat: React Query 전역 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import { RouterProvider } from 'react-router-dom'
 import { router } from './router/router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
-
     return (
         <>
-            <RouterProvider router={router} />
+            <QueryClientProvider client={queryClient}>
+                <RouterProvider router={router} />
+            </QueryClientProvider>
         </>
     )
 }
