27b063ae370fb08e31a439c9c466c19ac484eb8d
sejeong223 <sejeonggim68@gmail.com>
2025-08-01 00:27:39 +0900

    refactor: useMutation 관련 hook을 const로 리팩토링

--- frontend/src/hooks/mutations/userMutations.ts
+++ frontend/src/hooks/mutations/userMutations.ts
@@ -1,19 +1,19 @@
 import { useMutation } from '@tanstack/react-query'
 import { patchMemberAgree, patchMemberProfileImage, patchMemberSNS } from '../../api/user'
 
-export function usePatchMemberAgree() {
+export const usePatchMemberAgree = () => {
     return useMutation({
         mutationFn: patchMemberAgree,
     })
 }
 
-export function usePatchMemberSNS() {
+export const usePatchMemberSNS = () => {
     return useMutation({
         mutationFn: patchMemberSNS,
     })
 }
 
-export function usePatchMemberProfileImage() {
+export const usePatchMemberProfileImage = () => {
     return useMutation({
         mutationFn: patchMemberProfileImage,
     })
