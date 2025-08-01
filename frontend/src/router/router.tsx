78b4abf3f432d4b26e64025ac1463135b75c14aa
띵 <wkdaudns12@gmail.com>
2025-08-01 18:31:09 +0900

    fix: settingPage 라우팅 제거

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -29,10 +29,6 @@ export const router = createBrowserRouter([
                 element: <LibraryPage />,
             },
             {
-                path: '/setting',
-                element: <SettingPageRoute />,
-            },
-            {
                 path: '/auth/callback',
                 element: <GoogleLoginRedirectPage />,
             },
