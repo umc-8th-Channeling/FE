579d91588270cf1fd847393eaf321c932dfce1f4
yoonvinjeong <anaabc1004@naver.com>
2025-08-03 21:50:41 +0900

    fix: 페이지네이션 높이 조정

--- frontend/src/pages/my/_components/videolist.tsx
+++ frontend/src/pages/my/_components/videolist.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import MyVideoCard from './myVideoCard'
 import MyShortsCard from './myShortsCard'
 import Pagination from '../../../components/Pagination'
@@ -17,10 +17,19 @@ export default function Videolist() {
         activeTab === 'video' ? (videoCurrentPage - 1) * itemsPerPage : (shortsCurrentPage - 1) * itemsPerPage
 
     const data = activeTab === 'video' ? videosData : shortsData
+    const currentPage = activeTab === 'video' ? videoCurrentPage : shortsCurrentPage
     const currentItems = data.slice(offset, offset + itemsPerPage)
 
     const currentLength = currentItems.length
     const emptyCount = itemsPerPage - currentLength
+
+    useEffect(() => {
+        const totalPages = Math.ceil(data.length / itemsPerPage)
+        if (currentPage > totalPages && totalPages > 0) {
+            if (activeTab === 'video') setVideoCurrentPage(totalPages)
+            else setShortsCurrentPage(totalPages)
+        }
+    }, [data.length, itemsPerPage, currentPage])
 
     return (
         <div className="flex flex-col w-full items-start content-start pb-[80px] gap-[16px]">
@@ -48,44 +57,16 @@ export default function Videolist() {
                 </button>
             </div>
             {activeTab === 'video' && (
-                <div className="grid grid-cols-2 desktop:grid-cols-4 w-full desktop:min-h-[744px] min-h-[1560px] self-stretch gap-4 tablet:gap-6 cursor-pointer">
+                <div className="grid grid-cols-2 desktop:grid-cols-4 w-full self-stretch gap-4 tablet:gap-6 cursor-pointer">
                     {currentItems.map((video) => (
                         <MyVideoCard video={video} key={video.id} />
-                    ))}
-                    {Array.from({ length: emptyCount }).map((_, i) => (
-                        <div
-                            key={`empty-${i}`}
-                            className="flex flex-col h-fit items-center gap-[8px] w-full"
-                            aria-hidden // 접근성: 스크린리더 무시
-                        >
-                            <div className="w-full aspect-[141/79] shrink-0 rounded-[8px] bg-transparent" />
-                            <div className="w-full flex flex-col h-[78px] items-start gap-[4px]">
-                                <div className="self-stretch text-transparent text-[16px] tablet:text-[18px] font-bold leading-[140%] tracking-[-0.45px]"></div>
-                                <div className="self-stretch text-transparent text-[12px] tablet:text-[14px] font-normal leading-[140%] tracking-[-0.35px]">
-                                    -
-                                </div>
-                            </div>
-                        </div>
                     ))}
                 </div>
             )}
             {activeTab === 'shorts' && (
-                <div className="grid grid-cols-3 desktop:grid-cols-6 w-full desktop:min-h-[766px] min-h-[1556px] self-stretch desktop:gap-x-4 gap-x-[9px] gap-y-6 cursor-pointer">
+                <div className="grid grid-cols-3 desktop:grid-cols-6 w-full  self-stretch desktop:gap-x-4 gap-x-[9px] gap-y-6 cursor-pointer">
                     {currentItems.map((short) => (
                         <MyShortsCard shorts={short} key={short.id} />
-                    ))}
-                    {Array.from({ length: emptyCount }).map((_, i) => (
-                        <div
-                            key={`empty-${i}`}
-                            className="flex flex-col h-fit items-center tablet:gap-x-[9px] desktop:gap-[8px] shrink-0"
-                            aria-hidden
-                        >
-                            <div className="w-full aspect-[192/289] rounded-[8px]"></div>
-                            <div className="w-full gap-1 mt-[8px] h-[74px] items-start">
-                                <div className=" text-transparent text-[16px] tablet:text-[18px] font-bold leading-[140%] tracking-[-0.45px] "></div>
-                                <div className=" text-transparent text-[12px] tablet:text-[14px] font-normal leading-[140%] tracking-[-0.35px]"></div>
-                            </div>
-                        </div>
                     ))}
                 </div>
             )}
