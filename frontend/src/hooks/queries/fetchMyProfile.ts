33c3df3c7ddcb371db1f31a7a732f55b3b1666ad
sejeong223 <sejeonggim68@gmail.com>
2025-08-12 20:50:31 +0900

    fix: 프로필 데이터 무한 캐싱 적용

--- frontend/src/hooks/queries/fetchMyProfile.ts
+++ frontend/src/hooks/queries/fetchMyProfile.ts
@@ -11,6 +11,6 @@ export const useFetchMyProfile = () => {
     return useQuery({
         queryKey: ['my-profile'],
         queryFn: fetchMyProfile,
-        staleTime: 1000 * 60 * 5,
+        staleTime: Infinity,
     })
 }
