58f6d07709abc98a64c9784b4ac50c005197c2e5
Sowon Kim <sso626262@naver.com>
2025-08-15 01:07:06 +0900

    chore: 배포 환경용 axios baseURL 설정

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -3,12 +3,12 @@ import { LOCAL_STORAGE_KEY } from '../constants/key'
 import { useLocalStorage } from '../hooks/useLocalStorage'
 
 export const axiosInstance = axios.create({
-    withCredentials: true,
-    baseURL: '/api', //개발 환경에서
-    // baseURL: import.meta.env.VITE_SERVER_API_URL,
+    // withCredentials: true,
+    // baseURL: '/api', //개발 환경에서
+    baseURL: import.meta.env.VITE_SERVER_API_URL,
 })
 
-//요청 인터셉터
+// 요청 인터셉터
 axiosInstance.interceptors.request.use((config) => {
     const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken)
     const token = getItem()
