2d9674d6536439c17c0d861109e5e7ef585e3d0c
Sowon Kim <sso626262@naver.com>
2025-08-15 12:44:34 +0900

    chore: 내 채널 영상 리스트 예외 처리 및 로딩 추가

--- frontend/src/api/my.ts
+++ frontend/src/api/my.ts
@@ -10,8 +10,6 @@ import { axiosInstance } from './axios'
 
 export const getChannelDetail = async ({ channelId }: RequestChannelDto): Promise<ResponseChannelDto> => {
     const { data } = await axiosInstance.get(`/channels/${channelId}`)
-    console.log('📦 채널 상세 응답:', data)
-
     return data
 }
 
@@ -28,12 +26,10 @@ export const getChannelVideo = async ({
             ...(size !== undefined && { size }),
         },
     })
-    console.log('📦 채널 비디오 상세 응답:', data)
     return data
 }
 
 export const postMyVideoReport = async ({ videoId }: MyVideoReportDto): Promise<ResponseMyVideoReportDto> => {
     const { data } = await axiosInstance.post<ResponseMyVideoReportDto>(`/reports/${videoId}`)
-    console.log('📦 채널 비디오 상세 응답   :', data)
     return data
 }
